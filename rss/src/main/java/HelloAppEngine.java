30212237dab3d3ef8a8753075059a6adf427496e
YuriyMilov <GitHub@quicklydone.com>
2021-09-17 11:40:27 -0400

    q

--- rss/src/main/java/HelloAppEngine.java
+++ rss/src/main/java/HelloAppEngine.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 )
 public class HelloAppEngine extends HttpServlet {
 
-  @Override
+  @Override 
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
 
